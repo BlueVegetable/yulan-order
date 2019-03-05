497645943a5e552fd718b7fe362f6fd2f3c3d97d
BlueVegetable <2601042086@qq.com>
2019-03-05 16:14:37 +0800

    修改了活动查询的bug

--- src/main/java/com/yulan/controller/SalPromotionController.java
+++ src/main/java/com/yulan/controller/SalPromotionController.java
@@ -31,24 +31,9 @@ public class SalPromotionController {
 
     @ResponseBody@RequestMapping("getSalPromotionsByIDs")
     public List<String> getSalPromotionsByIDs(@RequestBody List<String> salPromotionIDs) {
-        Set<String> salPromotionIDsDeal = new LinkedHashSet<>();
-        for (String salPromotion:salPromotionIDs) {
-            salPromotionIDsDeal.add(salPromotion);
-        }
-        List<String> salPromotionIDsUse = new ArrayList<>();
-        for (String salPromotionID:salPromotionIDsDeal) {
-            salPromotionIDsUse.add(salPromotionID);
-        }
-        List<String> data = salPromotionService.getSalPromotionNamesByIDs(salPromotionIDsUse);
         List<String> result = new ArrayList<>();
-        int j=0;
         for (String salPromotionID:salPromotionIDs) {
-            String value = null;
-            if(!salPromotionID.equals(salPromotionIDsUse.get(j))) {
-                j++;
-            }
-            value = data.get(j);
-            result.add(value);
+            result.add(salPromotionService.getSalPromotionByID(salPromotionID).getOrderName());
         }
         return result;
     }
