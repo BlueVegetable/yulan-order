d5309a739f4b0f6d84e45e91c912b4bf117c70ec
AnnieTYF <980315703@qq.com>
2019-03-07 17:53:37 +0800

    postAddress

--- src/main/java/com/yulan/pojo/PostAddress.java
+++ src/main/java/com/yulan/pojo/PostAddress.java
@@ -12,7 +12,37 @@ public class PostAddress {
 
     private Integer addressId;
 
+    private String province;
+
+    private String city;
+
+    private String country;
+
     public PostAddress() {
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public Integer getAddressId() {
