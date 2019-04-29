6122321e6548017f6a92d768af3ee2b47407604c
BlueVegetable <2601042086@qq.com>
2019-04-29 20:51:15 +0800

    提高了查询购物车的速度

--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -53,7 +53,7 @@ public class ItemTest {
     public void test1() {
         List<String> cartItemIDs = new ArrayList<>();
         cartItemIDs.add("1556249842365557d56147bbd4c6199f31ec839816172");
-        System.out.println(commodityOrderService.submitCommodityOrder(cartItemIDs));
+//        System.out.println(commodityOrderService.submitCommodityOrder(cartItemIDs));
     }
     @Test
     public void test2() {
