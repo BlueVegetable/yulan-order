b5d2648a5ed4a2f8631986073c8d258f9c867fe0
BlueVegetable <2601042086@qq.com>
2019-07-09 15:34:00 +0800

    成功修改了salPromotion的问题

--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -1,19 +1,17 @@
-import com.yulan.service.CommodityService;
+import com.yulan.dao.SalPromotionDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.math.BigDecimal;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class ItemTest {
     @Autowired
-    private CommodityService commodityService;
+    private SalPromotionDao salPromotionDao;
     @Test
     public void test1() {
-        commodityService.alterCommodityPrice("1560340254184a0eff79299a04238906e1e577d841d29",new BigDecimal("1"));
+        System.out.println(salPromotionDao.getSalPromotionByID("2019"));
     }
 }
