cdb725e02751b7a7ef9558284aed1bd61220d313
BlueVegetable <2601042086@qq.com>
2019-07-09 20:42:29 +0800

    修改了提交订单的三个flag的问题

--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -1,17 +1,26 @@
-import com.yulan.dao.SalPromotionDao;
+import com.yulan.service.CommodityOrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class ItemTest {
     @Autowired
-    private SalPromotionDao salPromotionDao;
+    private CommodityOrderService commodityOrderService;
     @Test
     public void test1() {
-        System.out.println(salPromotionDao.getSalPromotionByID("2019"));
+        List<String> cartItemIDs = new ArrayList<>();
+        cartItemIDs.add("15624003127520cf306061ec74a7190815bcb44557260");
+        Map<String,Integer> lineNos = new HashMap<>();
+        lineNos.put("15624003127520cf306061ec74a7190815bcb44557260",1);
+        System.out.println(commodityOrderService.submitCommodityOrder(cartItemIDs,lineNos));
     }
 }
